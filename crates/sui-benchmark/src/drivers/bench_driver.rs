87306c794165d488d930e6c5f7744961466d2821
sadhan <sadhan@mystenlabs.com>
2022-09-19 15:27:09 -0700

    Fix how we handle control-c in stress

--- crates/sui-benchmark/src/drivers/bench_driver.rs
+++ crates/sui-benchmark/src/drivers/bench_driver.rs
@@ -18,6 +18,7 @@ use prometheus::IntCounterVec;
 use prometheus::Registry;
 use sui_core::authority_aggregator::AuthorityAggregator;
 use tokio::sync::OnceCell;
+use tokio_util::sync::CancellationToken;
 
 use crate::drivers::driver::Driver;
 use crate::drivers::HistogramWrapper;
@@ -127,6 +128,7 @@ pub struct BenchWorker {
 pub struct BenchDriver {
     pub stat_collection_interval: u64,
     pub start_time: Instant,
+    pub token: CancellationToken,
 }
 
 impl BenchDriver {
@@ -134,7 +136,11 @@ impl BenchDriver {
         BenchDriver {
             stat_collection_interval,
             start_time: Instant::now(),
+            token: CancellationToken::new(),
         }
+    }
+    pub fn terminate(&self) {
+        self.token.cancel()
     }
     pub fn update_progress(
         start_time: Instant,
@@ -224,6 +230,7 @@ impl Driver<BenchmarkStats> for BenchDriver {
                 ),
         });
         for (i, worker) in bench_workers.into_iter().enumerate() {
+            let cloned_token = self.token.clone();
             let request_delay_micros = 1_000_000 / worker.target_qps;
             let mut free_pool = worker.payload;
             let progress = progress.clone();
@@ -254,7 +261,7 @@ impl Driver<BenchmarkStats> for BenchDriver {
                 let mut stat_start_time: Instant = Instant::now();
                 loop {
                     tokio::select! {
-                            _ = tokio::signal::ctrl_c() => {
+                            _ = cloned_token.cancelled() => {
                                 break;
                             }
                             _ = stat_interval.tick() => {
@@ -491,7 +498,14 @@ impl Driver<BenchmarkStats> for BenchDriver {
             benchmark_stat
         });
         drop(tx);
-        let _res: Vec<_> = try_join_all(tasks).await.unwrap().into_iter().collect();
+        let all_tasks = try_join_all(tasks);
+        let _res = tokio::select! {
+            _ = tokio::signal::ctrl_c() => {
+                self.terminate();
+                vec![]
+            }
+            res = all_tasks => res.unwrap().into_iter().collect()
+        };
         let benchmark_stat = stat_task.await.unwrap();
         Ok(benchmark_stat)
     }
